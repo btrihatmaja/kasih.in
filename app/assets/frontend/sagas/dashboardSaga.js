fd6de7f3e24e3723838908abdf86acf325987074
Bagus Trihatmaja <astungkara.project@gmail.com>
2017-01-01 01:49:55 +0800

    Fix string interpolation in dashboard.

--- app/assets/frontend/sagas/dashboardSaga.js
+++ app/assets/frontend/sagas/dashboardSaga.js
@@ -7,7 +7,7 @@ import * as sidebarIndexTypes from '../constants/sidebarIndexTypes';
 
 function* updateURL(action) {
   const { slug } = action;
-  yield put(push('/dashboard/${slug}'));
+  yield put(push(`/dashboard/${slug}`));
 }
 
 function* updateSidebar(action) {
